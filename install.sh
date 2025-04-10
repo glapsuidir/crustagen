ec5eb37454ce91cf91e03722ec7bcb8c7485ba0e
Glapsuidir <jamiejtoone@gmail.com>
2025-04-09 17:34:51 -0700

    Fix install.sh script to overwrite if never version is available

--- install.sh
+++ install.sh
@@ -3,31 +3,27 @@
 set -e
 
 VERSION="v0.1.1-alpha"
+INSTALL_DIR="/usr/local/bin"
+BIN_NAME="crustagen"
+RELEASE_URL="https://github.com/glapsuidir/crustagen/releases/download/$VERSION/$BIN_NAME"
 
-echo "Fetching the latest release (including pre-releases) from GitHub..."
+echo "Fetching the latest release from GitHub..."
 echo "Latest version: $VERSION"
 
-RELEASE_URL="https://github.com/glapsuidir/crustagen/releases/download/v0.1.1-alpha/crustagen"
-
-if [ "$VERSION" == "null" ]; then
-    echo "Error: No valid release version found. Exiting."
-    exit 1
-fi
-
 echo "Downloading $VERSION..."
-curl -L $RELEASE_URL -o crustagen
+curl -fL "$RELEASE_URL" -o "$BIN_NAME" || {
+    echo "Failed to download binary. Does the release asset exist at $RELEASE_URL?"
+    exit 1
+}
 
-chmod +x crustagen
+chmod +x "$BIN_NAME"
 
-INSTALL_DIR="/usr/local/bin"
-
-if [ ! -f "$INSTALL_DIR/crustagen" ]; then
-    sudo mv crustagen "$INSTALL_DIR/"
-
-    sudo chmod +x "$INSTALL_DIR/crustagen"
-    echo "Installation complete! You can run Crustagen by typing in 'crustagen' followed by any relevant flags. Try crustagen --help for more info."
-else
-    echo "Crustagen is already installed in $INSTALL_DIR, installing latest version..."
+if [ -f "$INSTALL_DIR/$BIN_NAME" ]; then
+    echo "Crustagen is already installed in $INSTALL_DIR. Overwriting..."
+    sudo rm "$INSTALL_DIR/$BIN_NAME"
 fi
 
-sudo mv -f crustagen /usr/local/bin/crustagen
+sudo mv "$BIN_NAME" "$INSTALL_DIR/"
+sudo chmod +x "$INSTALL_DIR/$BIN_NAME"
+
+printf "Installation complete!\nYou can run Crustagen by typing 'crustagen'. Try 'crustagen --help' for more\ninfo.\n"
